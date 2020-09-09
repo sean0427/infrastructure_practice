09926b63b1db7a1a4e15d06be6caefecf8679efb
Sean Guan <sean520427@gmail.com>
2020-09-09 20:08:04 +0800

    rename order_systen lambda directory

--- order_system/setup.py
+++ order_system/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import pathlib
 
 setup(
-        name='price_lambda',
+        name='order_system',
         version='0.0.1',
         packages=find_packages(where='hanlder'),
         install_requires=[]
